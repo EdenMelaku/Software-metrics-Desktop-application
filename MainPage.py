25fd08b7d0bb4a8e23dc8a811c8454878d5a5a55
eden melaku <edenzzzmelaku@gmail.com>
2019-04-19 22:55:50 +0300

    main page updated

--- MainPage.py
+++ MainPage.py
@@ -1,13 +1,17 @@
-
-
 from tkinter import *
 
 window = Tk()
 
 window.title("SOFTWARE METRICS APPLICATION")
 
-lbl = Label(window, text="Hello sdhfixjkls ksdhfileuhf aksufdh")
+window.geometry('350x200')
 
-lbl.grid(column=10, row=10)
+lbl = Label(window, text="welcome to software metrics application")
+
+lbl.grid(column=0, row=0)
+
+btn = Button(window, text="Click Me")
+
+btn.grid(column=1, row=0)
 
 window.mainloop()
