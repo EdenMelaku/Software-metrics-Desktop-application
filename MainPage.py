082550523dc9dea387a77f690129e7eb6365b77c
eden melaku <edenzzzmelaku@gmail.com>
2019-04-19 22:53:15 +0300

    main page added -- just trying tkinter

---
+++ MainPage.py
@@ -0,0 +1,13 @@
+
+
+from tkinter import *
+
+window = Tk()
+
+window.title("SOFTWARE METRICS APPLICATION")
+
+lbl = Label(window, text="Hello sdhfixjkls ksdhfileuhf aksufdh")
+
+lbl.grid(column=10, row=10)
+
+window.mainloop()
