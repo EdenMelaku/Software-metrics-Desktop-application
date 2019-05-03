568f0129e48f5c40a53b90658d3dd96cff5f55b4
eden melaku <edenzzzmelaku@gmail.com>
2019-05-04 01:25:01 +0300

    code updated to add a language chooser combobox

--- MainPage.py
+++ MainPage.py
@@ -1,8 +1,9 @@
 import tkinter
 from tkinter import *
 from tkinter import filedialog
+from tkinter.ttk import Combobox
 
-from app import returnFiles, toPage
+#from app import returnFiles, toPage
 
 window = Tk()
 
@@ -20,15 +21,21 @@ lbl = Label(window, text="welcome to software metrics application")
 
 lbl.grid(column=0, row=0)
 
-open = tkinter.Button(window, text = "Open", command = open_Dir_chooser())
+open = tkinter.Button(window, text = "Open", command=open_Dir_chooser())
 
 
 open.grid(column=1, row=0)
 
 # defining open_file_chooser function
+combo = Combobox(window)
 
+combo['values'] = ("java", "python", "c++")
+
+combo.current(1)  # set the selected item
+
+combo.grid(column=0, row=0)
 
 window.mainloop()
-files, dir=returnFiles(Dir)
-rep=toPage(dir,files)
+#files, dir=returnFiles(Dir)
+#rep=toPage(dir,files)
 
