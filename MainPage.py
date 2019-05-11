02697071c20c3a79d48573cac71b02575c86410f
eden melaku <edenzzzmelaku@gmail.com>
2019-05-12 00:32:36 +0300

    code fixed

--- MainPage.py
+++ MainPage.py
@@ -4,6 +4,7 @@ from tkinter import filedialog, scrolledtext
 from tkinter.ttk import Combobox
 
 #from app import returnFiles, toPage
+import app
 from app import returnFiles, toPage
 
 window = Tk()
@@ -17,20 +18,44 @@ def open_Dir_chooser():
     global Dir
     Dir=ProjDir
     print("You have selected : %s" % ProjDir)
+    txt.config(state='normal')
+    txt.delete('1.0', END)
+
 
 lbl = Label(window, text="welcome to software metrics application")
 
 lbl.grid(column=0, row=0)
 
+
+
+lbl = Label(window, text=" analysis report ")
+
+lbl.grid(column=3, row=0)
+
+
 open = tkinter.Button(window, text = "Choose project folder", command=open_Dir_chooser)
 
 
-open.grid(column=3, row=0)
+open.grid(column=0, row=2)
 
 
-txt = scrolledtext.ScrolledText(window,width=140,height=30)
+txt = scrolledtext.ScrolledText(window,width=100,height=30)
 
-txt.grid(column=4, row=6)
+txt.grid(column=3, row=6)
+
+
+
+
+txtt = scrolledtext.ScrolledText(window,width=50,height=30)
+
+txtt.grid(column=0, row=6)
+
+
+
+
+
+
+txtt.config(state='disable')
 
 
 # defining open_file_chooser function
@@ -43,15 +68,21 @@ combo.current(1)  # set the selected item
 
 combo.grid(column=0, row=3)
 def send():
+ txt.config(state='normal')
+ app.initReport(Dir)
+ x="GENERATING REPORT FOR THE PROJECT FOLDER    "+Dir+"\n\n\n";
+ txt.insert(INSERT, x)
+
  files, dir=returnFiles(Dir)
  print(combo.get())
  print("INSIDE METHOD")
  rep=toPage(dir,files,combo.get())
  txt.insert(INSERT, rep)
+ txt.config(state='disable')
 
 open = tkinter.Button(window, text = "analyse", command=send)
 
 
-open.grid(column=3, row=3)
+open.grid(column=0, row=4)
 window.mainloop()
 
